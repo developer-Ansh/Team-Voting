63c54cf11b2579c6335ad3a1e6ffaaa8c68dd8e7
developer-Ansh <ganshgamer@gmail.com>
2021-11-23 18:45:27 +0530

    changed styling

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAIwzlrgAMQJJf6tHT_yZFk81r6sjrF1Q4",
+  authDomain: "pro-67-57702.firebaseapp.com",
+  projectId: "pro-67-57702",
+  storageBucket: "pro-67-57702.appspot.com",
+  messagingSenderId: "438520655549",
+  appId: "1:438520655549:web:e4704fbb6f5be327fb91d4"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
